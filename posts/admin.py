e595ce080ad6a70d163a3eec5d814d5f60588b2d
kali <kali@kali.kali>
2023-11-06 00:07:37 +0200

    add admin customization

--- posts/admin.py
+++ posts/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from .models import Post
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['title','draft']
+    list_filter=['draft']
+    search_fields=['title']
 
 
 
-admin.site.register(Post)
+admin.site.register(Post,ProductAdmin)
--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=20000)
     draft = models.BooleanField(default=True)
+    def __str__(self):
+        return self.title
