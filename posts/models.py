0c45b27e5791fc10cec3f2b64d1dd57818656e0e
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2023-11-08 05:52:07 +0200

    add author to models

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 
 
 
@@ -12,7 +13,12 @@ class Post(models.Model):
     draft = models.BooleanField(default=True)
     publish_date= models.DateTimeField(default = timezone.now)
     tags = TaggableManager()
-    imag=models.ImageField(upload_to='posts')
+    imag=models.ImageField(upload_to='post')
+    author = models.ForeignKey(User,related_name='post_author',on_delete=models.CASCADE)
+
+
+
+
 
     def __str__(self):
         return self.title
