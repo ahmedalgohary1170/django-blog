451d86e03971913720dd9150801f775756c8b1ae
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2023-11-10 20:16:26 +0200

    create graph for db models

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'taggit',
     'posts',
+    'django_extensions',
     
 ]
 
@@ -132,3 +133,7 @@ MEDIA_ROOT=BASE_DIR / "media"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+GRAPH_MODELS = {
+  'all_applications': False,
+  'group_models': True,
+}
