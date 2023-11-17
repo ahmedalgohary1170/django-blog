ab5272dc62b10dde103d261b8b9dd16159b36369
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2023-11-17 11:01:19 +0200

    post author in save

--- posts/forms.py
+++ posts/forms.py
@@ -3,5 +3,5 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = '__all__'
-        
+        # fields = '__all__'
+        exclude =('author',)
--- posts/views.py
+++ posts/views.py
@@ -30,7 +30,9 @@ def create_post(request):
     if request.method == 'POST':
         form =PostForm(request.POST,request.FILES)
         if form.is_valid():
-            form.save()
+            myform=form.save(commit=False)
+            myform.author = request.user
+            myform.save()
             return redirect('/posts/')
     else:
         form = PostForm()
