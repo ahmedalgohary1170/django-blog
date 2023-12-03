a930f60d894e284f4d0ef319f4eeace01dfe18f4
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2023-12-03 19:54:23 +0200

    add swagger docs

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django_summernote', 
     "django_bootstrap5",
     'rest_framework',
+    'drf_yasg',
     
 ]
 
--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,30 @@ from posts.views import post_list,post_detail,PostList,PostDetail,create_post,Ad
 from posts.api import PostListApi,PostDetailApi
 
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="BLOG API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,7 +60,13 @@ urlpatterns = [
 
 
     path('posts/api', PostListApi.as_view()),
-    path('posts/api/<int:pk>',PostDetailApi.as_view())
+    path('posts/api/<int:pk>',PostDetailApi.as_view()),
+
+
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 
